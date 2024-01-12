5ea144dd1c418da340fe27d4a94d6e92c845f31f
Hayden Lymath <hayden.lymath@live.com>
2024-01-12 14:19:52 +0000

    kerberos auth steps

---
+++ proxy/gen_keytab.bash
@@ -0,0 +1,4 @@
+ktutil
+addent -password -p HTTP/server.example.com@YOUR.AD.REALM -k 1 -e aes256-cts
+wkt /path/to/HTTP.keytab
+quit
---
+++ proxy/secure_keytab.bash
@@ -0,0 +1,2 @@
+chown root:nginx /path/to/HTTP.keytab
+chmod 640 /path/to/HTTP.keytab
